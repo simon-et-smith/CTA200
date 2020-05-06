fc17e900c88c7571d69975869558908e09d6c8cf
Simon <set.smith@mail.utoronto.ca>
2020-05-06 16:53:11 -0400

    Added comments to main.c and updated history.txt

--- assignment1/main.c
+++ assignment1/main.c
@@ -6,7 +6,7 @@ int main() {
     double arr[] = {1.0, 2.0, 3.0, 4.0};
 
     double result = average(4, arr);
-
+    /* Added the negative sum statement, isn't that neat*/
     printf("The average of 1, 2, 3, and 4 is: %.4f\n", result);
     printf("Conversly, the average of -1, -2, -3, and -4 is: -%.4f\n", result);
     return 0;    
